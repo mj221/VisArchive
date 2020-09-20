4c69901b8c088d1fd45f37eaa39a6b7b6518810c
MJ LEE <mj1201276@yahoo.com.au>
2020-09-20 11:16:42 +1000

    Added codeigniter files

---
+++ htdocs/codeigniter/application/models/Profile_model.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profile_model extends CI_Model {
+    public function __construct() {
+        $this->load->database();
+        
+    }
+    
+    public function get_my_video($ID) {
+
+        $query = $this->db->get_where('videos',array('UID' => $ID));
+
+        return $query->result_array();
+    }
+}
